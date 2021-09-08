8eba3f96a58a4759a8d4ccd142cb696c549f7ba9
Julian Edington <julian.edington@outlook.com>
2021-09-08 11:30:06 -0500

    added -E switch in controller setup script

--- Ansible/controller-ubuntu-setup.sh
+++ Ansible/controller-ubuntu-setup.sh
@@ -2,6 +2,6 @@
 # example commands to setup controller
 sudo apt-get update
 sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:ansible/ansible
+sudo -E apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible -y
