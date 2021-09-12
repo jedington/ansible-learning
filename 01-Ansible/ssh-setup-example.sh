32f3d7dac7dcbc4f60a52031fb58fa566a936f8b
Julian Edington <julian.edington@outlook.com>
2021-09-11 19:24:11 -0500

    changed localhost specification

--- 01-Ansible/ssh-setup-example.sh
+++ 01-Ansible/ssh-setup-example.sh
@@ -4,7 +4,7 @@
 # example commands to setup ssh--might need to change/add root@'s
 
 ssh-keygen -t rsa -b 4096 -C "Ansible Key"
-ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@localhost
+ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@controller
 ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@node1
 ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@node2
 ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@node3
