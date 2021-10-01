174551011e380e89afe3e2a06e3760b1d96550de
Julian Edington <julian.edington@outlook.com>
2021-09-30 19:08:45 -0500

    comments, no prompt ssh-keygen

--- 01-Ansible/ssh-setup-example.sh
+++ 01-Ansible/ssh-setup-example.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# this is optional
+# this is optional # passwords for vagrant username are: vagrant
 # example commands to setup ssh--might need to change/add root@'s
 
-ssh-keygen -t rsa -b 4096 -C "Ansible Key"
+ssh-keygen -t rsa -b 4096 -C "Ansible Key" -N '' <<<$'\n'
 ssh-copy-id -i $HOME/.ssh/id_rsa.pub vagrant@controller
 ssh-copy-id -i $HOME/.ssh/id_rsa.pub vagrant@node1
 ssh-copy-id -i $HOME/.ssh/id_rsa.pub vagrant@node2
@@ -13,6 +13,3 @@ ssh-copy-id -i $HOME/.ssh/id_rsa.pub vagrant@dev3
 eval $(ssh-agent)
 ssh-add
 ansible -i ~/etc/hosts -m ping all
-
-#- sudo ansible-playbook -i ~/etc/ansible/hosts all-setup.yml
-# ^ run after setting up ssh/other auth options ^
